b0303d6d75fa0cf7c320c6aa0be4ae01f01c6700
alexis123nieto <alexys13@live.com.mx>
2017-03-24 08:11:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
         $db_host = "am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         $db_name = "zxl4lpl8325rqc9l";
         $db_user = "j2qv3f0n8u3tzlnr";
-        $db_password = "l94l5lhblg7e21ds";
+        $db_password = "qzy4hgt7qyb5ljip";
     
-      $connection = mysqli_connect('am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'j2qv3f0n8u3tzlnr', 'l94l5lhblg7e21ds');
+      $connection = mysqli_connect('am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'j2qv3f0n8u3tzlnr', 'qzy4hgt7qyb5ljip');
     
      mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
     @mysqli_query("SET NAMES 'utf8'");
