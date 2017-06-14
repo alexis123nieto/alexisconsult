e9b9037d75a15d6949dbabc8ab0eff47b2581e5e
alexis123nieto <alexys13@live.com.mx>
2017-06-14 15:32:42 -0500

    Create index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
     <?php
-      $db_host = "localhost";
-     $db_name = "panaderia";
-     $db_user = "root";
-     $db_password = "";
+      $db_host = "if0ck476y7axojpg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+     $db_name = "wpjkiorbuofp9ov9";
+     $db_user = "poa9o3zufh96lr39";
+     $db_password = "gz0zsymvbcrt7sia";
     
      $connection = mysqli_connect($db_host,$db_name, $db_password) or die("Connection Error: " . mysql_error());
      mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
     @mysqli_query("SET NAMES 'utf8'");
 
-    $sql_query = "SELECT * FROM inventario";
+    $sql_query = "SELECT * FROM  tbVentas";
     $result = mysqli_query($connection, $sql_query);
     $rows = array();
 while($r = mysqli_fetch_assoc($result)) {
