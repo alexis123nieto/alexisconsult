15e2a4cf14a5bf3d2afb593ac76c918af351053d
alexis123nieto <alexys13@live.com.mx>
2017-06-14 15:43:23 -0500

    Create index.php

--- index.php
+++ index.php
@@ -5,14 +5,24 @@
      $db_password = "gz0zsymvbcrt7sia";
     
      $connection = mysqli_connect($db_host,$db_name, $db_password) or die("Connection Error: " . mysql_error());
+
      mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
+
     @mysqli_query("SET NAMES 'utf8'");
 
-    $sql_query = "SELECT * FROM  tbVentas";
+
+
+    $sql_query = "SELECT * FROM tbVentas";
+
     $result = mysqli_query($connection, $sql_query);
+
     $rows = array();
+
 while($r = mysqli_fetch_assoc($result)) {
+
   $rows[] = $r;
+
 }
+
 print json_encode($rows);
 ?>
